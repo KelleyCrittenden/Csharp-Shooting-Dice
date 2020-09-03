531f6ca60dcb34d1027f91d9160d26c6965785af
Kelley Crittenden <kelleylcrittenden@gmail.com>
2020-09-03 08:36:50 -0500

    Finished Players 1-4

--- CreativeSmackTalkingPlayer.cs
+++ CreativeSmackTalkingPlayer.cs
@@ -1,8 +1,32 @@
+using System;
+using System.Collections.Generic;
+
 namespace ShootingDice
 {
     // A SmackTalkingPlayer who randomly selects a taunt from a list to say to the other player
-    public class CreativeSmackTalkingPlayer
+    public class CreativeSmackTalkingPlayer : Player
     {
+        public List<string> SmackTalkList = new List<string>()
+        {
+            "Thou forward unwash'd flirt-gill!",
+            "So, so, thou common dog, didst thou disgorge thy glutton bosom.",
+            "Come, you are a tedious fool. To the purpose.",
+            "We leak in your chimney!",
+            "Thou impertinent sheep-biting whey-face!",
+            "Thou art a disease that must be cut away."
+
+        };
+
+        public override int Roll()
+        {
+
+            int randomTaunt = new Random().Next(SmackTalkList.Count);
+            string random = SmackTalkList[randomTaunt];
+            Console.WriteLine($"{Name} yells - {random}");
+
+            // Return a random number between 1 and DiceSize
+            return new Random().Next(DiceSize) + 1;
+        }
 
     }
 }
--- SmackTalkingPlayer.cs
+++ SmackTalkingPlayer.cs
@@ -1,10 +1,20 @@
+using System;
+
 namespace ShootingDice
 {
     // TODO: Complete this class
 
     // A Player who shouts a taunt every time they roll dice
-    public class SmackTalkingPlayer
+    public class SmackTalkingPlayer : Player
     {
-        public string Taunt { get; }
+        public string Taunt { get; } = ("Out of my sight! Thou dost infect my eyes");
+
+        public override int Roll()
+        {;
+            Console.WriteLine($"{Name} yells - {Taunt}");
+            // Return a random number between 1 and DiceSize
+            return new Random().Next(DiceSize) + 1;
+        }
+
     }
 }
